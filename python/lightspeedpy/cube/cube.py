f2cf4b1d1fbeb62b2e406063238c1439a8759597
Jack Dinsmore <jtd@stanford.edu>
2026-06-29 13:53:56 -0700

    Division by flat

--- python/lightspeedpy/cube/cube.py
+++ python/lightspeedpy/cube/cube.py
@@ -2,13 +2,15 @@ import numpy as np
 from astropy.io import fits
 import numpy as np
 from ..cli import get_dataset
+FLAT_NAN_THRESHOLD = 0.1
 
 def cube(args):
     data_set = get_dataset(args)
     cube = np.zeros((data_set.num_frames(),data_set.image_shape[0], data_set.image_shape[1]))
     print(cube.shape)
     for i, frame in enumerate(data_set):
-        cube[i] = frame.image
+        cube[i] = frame.image / data_set.flat
+        cube[i][data_set.flat < FLAT_NAN_THRESHOLD] = np.nan
     
     primary_hdu = fits.PrimaryHDU(cube, header=data_set.header0)
     cube_hdu = fits.ImageHDU(data=cube, name="CUBE", header=data_set.header1)
